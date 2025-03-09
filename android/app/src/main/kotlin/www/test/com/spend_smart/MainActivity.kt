a3856f5828f0a6fbb0dc64cb1ff2c35cf8e948da
punitDT <punit@dharmatech.in>
2025-03-10 01:21:18 +0530

    Update: Removed FlutterSmsInboxPluginRegistrant and modified MainActivity

--- android/app/src/main/kotlin/www/test/com/spend_smart/MainActivity.kt
+++ android/app/src/main/kotlin/www/test/com/spend_smart/MainActivity.kt
@@ -1,12 +1,5 @@
 package www.test.com.spend_smart
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import com.test.com.spend_smart.FlutterSmsInboxPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        FlutterSmsInboxPluginRegistrant.registerWith(flutterEngine.plugins)
-    }
-}
+class MainActivity : FlutterActivity()
